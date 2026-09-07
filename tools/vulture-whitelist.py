15a74f96a1de09e7ec5101419cbcb4f5a7486319
Frank Niessink <frank@niessink.com>
2026-09-07 09:21:58 +0200

    Update dependencies

--- tools/vulture-whitelist.py
+++ tools/vulture-whitelist.py
@@ -1,3 +1,4 @@
+__call__  # unused function (src/update_time/domain/bound.py:25)
 __call__  # unused function (src/update_time/domain/dependency.py:21)
 tag_last_pushed  # unused variable (src/update_time/sources/docker_hub.py:59)
 download_url  # unused variable (src/update_time/sources/github.py:87)
